c3904a5072c5f3471ee628122e45eff81f067cea
Gustavo <Gustavo@LAPTOP-O6D53KO9>
2021-03-03 19:53:19 -0300

    Implementação das funcionalidades Drag and Drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [dropzoneEnabled, setDropzoneEnabled] = useState(false);
+  const [inDropZone, setInDropZone] = useState();
+
+  function dragstart({target}: any){
+    //console.log('Card: start Drag'); 
+    setDropzoneEnabled(true);
+    target.classList.add('isDragging');
+  }
+
+  function drag(){
+    //console.log('Card: is Dragging');
+  }
+
+  function dragend({target}: any){
+    //console.log('Card: end Drag');
+    setDropzoneEnabled(false);
+    target.classList.remove('isDragging');
+  }
+
+  function dragEnter(){
+    console.log('DropZone: Enter in drop Zone');
+  }
+
+  function dragOver({target}: any){
+    //console.log('DropZone: Over');
+    target.classList.add('over');
+
+    const cardDragging = document.querySelector('.isDragging');
+    target.appendChild(cardDragging);
+  }
+
+  function dragLeave({target}: any){
+    //console.log('DropZone: Leave');
+    target.classList.remove('over');
+  }
+
+  function drop({target}: any){
+    //console.log('DropZone: Drop');
+    target.classList.remove('over');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="boards">
+      <div className="board">
+        <h3>Todo</h3>
+        <div 
+          className={`${'dropzone'} ${dropzoneEnabled?'highlight':null} `}
+          onDragEnter={dragEnter}
+          onDragOver={dragOver}
+          onDragLeave={dragLeave}
+          onDrop={drop}
         >
-          Learn React
-        </a>
-      </header>
+          <div 
+            className="card" 
+            draggable="true"
+            onDragStart={dragstart}
+            onDrag={drag}
+            onDragEnd={dragend}
+          >
+            <div className="status blue"></div>
+            <div className="content">Fazer videos!</div>
+          </div>
+        </div>
+      </div>
+            
+      <div className="board">
+        <h3>In Progress</h3>
+        <div 
+          className={`${'dropzone'} ${dropzoneEnabled?'highlight':null} `}
+          onDragEnter={dragEnter}
+          onDragOver={dragOver}
+          onDragLeave={dragLeave}
+          onDrop={drop}
+        >
+          <div 
+            className="card" 
+            draggable="true"
+            onDragStart={dragstart}
+            onDrag={drag}
+            onDragEnd={dragend}
+          >
+            <div className="status green"></div>
+            <div className="content">Programando</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="board">
+        <h3>Done</h3>
+        <div 
+          className={`${'dropzone'} ${dropzoneEnabled?'highlight':null} `}
+          onDragEnter={dragEnter}
+          onDragOver={dragOver}
+          onDragLeave={dragLeave}
+          onDrop={drop}
+        >
+          <div 
+            className="card" 
+            draggable="true"
+            onDragStart={dragstart}
+            onDrag={drag}
+            onDragEnd={dragend}
+          >
+            <div className="status red"></div>
+            <div className="content">Responder Fórum</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
