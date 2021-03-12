2accf0e96195ed67bc6111b6d4175c88dab1b19a
Gustavo <Gustavo@LAPTOP-O6D53KO9>
2021-03-11 21:37:05 -0300

    Hours

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { timeStamp } from 'console';
 import { useEffect, useState } from 'react';
 import {Board} from './components/Board';
 import {Cards} from './components/Cards';
@@ -6,9 +5,9 @@ import { NavComponents } from './components/NavComponents';
 import './styles/App.css';
 function App() {
   //App não tem mais estados
-  const [time, setTime] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
   
 
 
@@ -16,28 +15,32 @@ function App() {
     setTimeout(()=>{
       setSeconds(seconds + 1);
     }, 1000);
-
-    if(seconds === 60){
-      setMinutes(1);
-      setSeconds(0);
-    }
   }, [seconds]);
- 
+  
+  if(seconds === 60){
+    setMinutes(minutes + 1);
+    setSeconds(0);
+  }
+
+  if(minutes === 60){
+    setHours(minutes + 1);
+    setMinutes(0);
+  }
   return (
     <div className="cont">
       <NavComponents/>
       <div className="boards">
 
-        <Board name="a fazer">
+        <Board name="To Do">
           <Cards hourInit={seconds} hourEnd={90} color="red" content="Terminar a aplicação"/>
         </Board> 
 
-        <Board name="em progresso">
-          <Cards hourInit={time} hourEnd={90} color="blue" content="Programando a aplicação"/>
+        <Board name="In progress">
+          <Cards hourInit={minutes} hourEnd={90} color="blue" content="Programando a aplicação"/>
         </Board> 
 
-        <Board name="feito">
-          <Cards hourInit={time} hourEnd={90} color="green" content="Aplicação final"/>
+        <Board name="Done">
+          <Cards hourInit={hours} hourEnd={90} color="green" content="Aplicação final"/>
         </Board>
       </div>
     </div>
