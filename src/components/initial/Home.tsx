1d92f20111d7582542e1a3d490aa5dcd4ffea409
Gustavo <Gustavo@LAPTOP-O6D53KO9>
2021-03-18 14:52:04 -0300

    Utilizando interface de Home do trello

--- src/components/initial/Home.tsx
+++ src/components/initial/Home.tsx
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom';
 import '../../styles/components/initial/Home.css';
 import { Navbar } from './NavBar';
 export default function Home(){
     return(
         <div className="HomeContent">
             <Navbar></Navbar>
+            <div className="Image">
+                <h2>O Trello ajuda os times a agilizar o trabalho.</h2>
+                <p>Colabore, gerencie projetos e alcance novos picos de produtividade. Seja em um arranha-céu ou em home office, o jeito de trabalhar do seu time é único, e o Trello pode ajudar você a produzir cada vez mais.</p>
+                <div className="cadUni">
+                    <input type="text" placeholder="Email"></input> <Link to="/cadastro" className="buttonAnimate">Cadastre-se. É grátis</Link>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/initial/NavBar.tsx
+++ src/components/initial/NavBar.tsx
@@ -1,34 +1,15 @@
 
 import '../../styles/components/initial/Navbar.css';
-import Logout from '../../images/logout.svg';
-
-import Gamepad from '../../images/gamepad.svg';
-import {useState } from 'react';
+import Trello from '../../images/Trello-Logo.svg';
 import { Link } from 'react-router-dom';
 
 
 
 export function Navbar(){
 
-  const [animateButton, setAnimateButton] = useState(false);
-
-  async function buttonAnimate({target}: any) {
-    animateButton?setAnimateButton(false):setAnimateButton(true);
-    await target.classList.add('buttonAnimate');
-  }
-
- async function buttonAnimateDown({target}: any) {
-    animateButton?setAnimateButton(false):setAnimateButton(true);
-    await target.classList.remove('buttonAnimate');
-  }
-
-  async function removeClass({target}:any) {
-    await target.classList.remove('buttonAnimate');
-  }
-
   return(
     <nav className="Navbar">
-      <img src={Gamepad} alt="Logo" className="logo" />
+      <img src={Trello} alt="Logo" className="logo" />
         <div className="navs">
           
         </div>
@@ -36,18 +17,15 @@ export function Navbar(){
         <div className="login">
             <Link 
                 to="/login"
-                onMouseEnter={buttonAnimate} 
-                onMouseLeave={buttonAnimateDown}
             >
-                Login <img src={Logout} onMouseMove={removeClass} alt="logout"/>
+                Fazer Login 
             </Link>
 
             <Link 
                 to="/cadastro"
-                onMouseEnter={buttonAnimate} 
-                onMouseLeave={buttonAnimateDown}
+                className="buttonAnimate"
             >
-                Cadastre-se <img src={Logout} onMouseMove={removeClass} alt="logout"/>
+                Cadastre-se
             </Link>
         </div>
     </nav>
