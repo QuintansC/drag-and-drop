6aa73c869fa127f5c863c79eb1cfc1cc184203e2
QuintansC <gustavosantos.2095@gmail.com>
2021-03-23 20:41:20 -0300

    Pagina cadastro

--- src/components/initial/Cadastro.tsx
+++ src/components/initial/Cadastro.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import '../../styles/components/initial/Cadastro.css';
+import Logo from './../../images/Trello-Logo.svg';
+import Google from './../../images/google.svg';
+import Microsoft from './../../images/Microsoft.svg';
+import Apple from './../../images/Apple.svg';
 
 
 export default function Login(props: any){
@@ -30,19 +34,21 @@ export default function Login(props: any){
 
     return(
         <div className="cadastroContent">
+            <img alt="logo" src={Logo}/>
             <div className="form">
                 <form method="POST">
-                    <span>Usuário</span>
-                    <input type="text" onChange={enviaForm}></input>
-                    <span>Senha</span>
-                    <input type="password" onChange={enviaForm}></input>
-                    <span>Confirmar Senha</span>
-                    <input type="password" onChange={enviaForm}></input>
-                    
-                    <input className="conectado" type="checkbox"></input>
-                    <span className="textConectado">Concordo com os termos e condições</span>
-                    <input className="submitButton" type="button" value="Entrar"></input>
-                </form>
+                    <h2>Crie sua conta</h2>
+                    <input type="text" onChange={enviaForm} placeholder="Insira seu e-mail"></input>
+                    <p>Ao se cadastrar, você confirma que leu e aceitou nossos Termos de serviço e nossa Política de privacidade.</p>
+                    <div>
+                        <button className="submitButtons" type="button" style={{marginTop: '50%', marginBottom: '-20%'}}>Conectar</button>
+                        <h3 className="textConectado">Ou</h3>                  
+                        <button className="submitButtons" type="button" ><img src={Google} alt=""/> Conectar com o Google</button>
+                        <button className="submitButtons" type="button" ><img src={Microsoft} alt=""/> Conectar com a Microsoft</button>
+                        <button className="submitButtons" type="button" ><img src={Apple} alt=""/> Conectar com a Apple</button>
+                    </div>
+               </form>
+               
             </div>
         </div>
     );
