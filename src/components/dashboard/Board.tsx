08ecf41b89b754fea4d36fa964b8bee8e1675bd0
QuintansC <gustavosantos.2095@gmail.com>
2021-03-27 18:52:57 -0300

    indent board

--- src/components/dashboard/Board.tsx
+++ src/components/dashboard/Board.tsx
@@ -10,42 +10,41 @@ interface BoardProps{
 
 
 export function Board({name, children}: BoardProps){
-
   //Funções da dropzone
   const { dropzoneEnabled } = useContext(DropzoneContext);
-    function dragEnter(){}
+  function dragEnter(){}
+  
+  function dragOver({target}: any){
+    target.classList.add('over');
+    try {
+      const cardDragging = document.querySelector('.isDragging');
+      target.appendChild(cardDragging);
+    } catch (error) {
+      //message error
+    }
     
-      function dragOver({target}: any){
-        target.classList.add('over');
-        try {
-          const cardDragging = document.querySelector('.isDragging');
-          target.appendChild(cardDragging);
-        } catch (error) {
-          
-        }
-        
-      }
-    
-      function dragLeave({target}: any){
-        target.classList.remove('over');
-      }
-    
-      function drop({target}: any){
-        target.classList.remove('over');
-      }
+  }
+  
+  function dragLeave({target}: any){
+    target.classList.remove('over');
+  }
 
-    return(
-      <div className="board">
-        <h3>{name}</h3>
-        <div 
-            className={`${'dropzone'} ${dropzoneEnabled?'highlight':null} `}
-            onDragEnter={dragEnter}
-            onDragOver={dragOver}
-            onDragLeave={dragLeave}
-            onDrop={drop}
-        >
-            {children}
-        </div>
+  function drop({target}: any){
+    target.classList.remove('over');
+  }
+
+  return(
+    <div className="board">
+      <h3>{name}</h3>
+      <div 
+        className={`${'dropzone'} ${dropzoneEnabled?'highlight':null} `}
+        onDragEnter={dragEnter}
+        onDragOver={dragOver}
+        onDragLeave={dragLeave}
+        onDrop={drop}
+      >
+        {children}
       </div>
-    )
+    </div>
+  )
 }
