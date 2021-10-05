a7b24545dc91fae526b008c2ddb7923aceae0b63
QuintansC <gustavosantos.2095@gmail.com>
2021-10-04 23:43:42 -0300

    refactor: styles from navHorizontal

--- src/components/dashboard/NavHorizontal.tsx
+++ src/components/dashboard/NavHorizontal.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../../styles/components/dashboard/NavHorizontal.module.css';
 
-export function NavHorizontal(){
+export function NavHorizontal(){ 
+
+  const [ishidden, setIsHidden] = useState(false)
+
+  function click(e: any){
+    e.preventDefault();
+    var objeto = document.querySelectorAll(`.${styles.project} li`)
+    if(ishidden){
+      console.log('teste')
+      objeto.forEach((obj)=>obj.removeAttribute('hidden'))
+      setIsHidden(false)
+    }else{
+      objeto.forEach((obj)=>obj.setAttribute('hidden', ''))
+      setIsHidden(true)
+    }
+  }
+
   return(
     <nav className={styles.navHorizontal}>
         <ul>
-            <li>Quadros</li>
-            <li>Modelos</li>
-            <li>Inicio</li>
+            <li><Link to="/">Quadros</Link></li>
+            <li><Link to="">Modelos</Link></li>
+            <li><Link to="">Inicio</Link></li>
         </ul>
         <h3>Area de trabalho</h3>
-        <ul>
-            <li>+ Criar area de trabalho</li>
+        <ul className={styles.project}>
+            <button onClick={click}> Projeto Name </button>
+            <li> teste</li>
+            <li> testes</li>
         </ul>
     </nav>
   )
