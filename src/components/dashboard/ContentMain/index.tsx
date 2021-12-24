c67a274f5054e89e16e7eab1879966d04136e833
QuintansC <gustavosantos.2095@gmail.com>
2021-12-24 03:10:53 -0300

    refactor: hide and show ContentMain Component

--- src/components/dashboard/ContentMain/index.tsx
+++ src/components/dashboard/ContentMain/index.tsx
@@ -1,10 +1,13 @@
 import styles from '../../../styles/components/dashboard/ContentMain.module.scss';
 import { FaRegClock } from 'react-icons/fa';
 import { Cards } from './cards';
+import { useContext } from 'react';
+import { DropzoneContext } from '../../../contexts/DropzoneContext';
 
 export function ContentMain(){ 
+  const { open } = useContext(DropzoneContext)
   return(
-    <div className={`${styles.HomeContent}`}>
+    <div className={open?styles.HomeContentOpened: styles.HomeContentClosed}>
       <span className={styles.titleSpan}> <FaRegClock size={25} /> <b>Visualizado recentemente</b></span>
       <div className={styles.row}>
           <Cards name="Projeto 1"/>
