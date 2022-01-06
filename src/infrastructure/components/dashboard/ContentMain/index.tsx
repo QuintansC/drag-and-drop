52c0ed79a835720a7e2ec473e77457102452a775
QuintansC <gustavosantos.2095@gmail.com>
2022-01-06 12:13:21 -0300

    feat: create new workspaces

--- src/infrastructure/components/dashboard/ContentMain/index.tsx
+++ src/infrastructure/components/dashboard/ContentMain/index.tsx
@@ -3,6 +3,7 @@ import { FaRegClock } from 'react-icons/fa';
 import { Cards } from './cards';
 import { useContext } from 'react';
 import { DropzoneContext } from '../../../../utils/contexts/DropzoneContext';
+import { Workspace } from './workspace';
 
 export function ContentMain(){ 
   const { open } = useContext(DropzoneContext)
@@ -10,11 +11,14 @@ export function ContentMain(){
     <div className={open?styles.HomeContentOpened: styles.HomeContentClosed}>
       <span className={styles.titleSpan}> <FaRegClock size={25} /> <b>Visualizado recentemente</b></span>
       <div className={styles.row}>
-          <Cards name="Projeto 1"/>
-          <Cards name="Projeto 2"/>
+        <Cards name="Projeto 1"/>
+        <Cards name="Projeto 2"/>
       </div>
-
       <span className={styles.workTitle}> <b>SUAS ÁREAS DE TRABALHO</b></span>
+      <Workspace nameWorkspace="Metrocomm" nameCard="Drag and Drop"/>
+      <Workspace nameWorkspace="Ancr" nameCard="Ancr Projetos" />
+      <span className={styles.workTitle}> <b> ÁREAS DE TRABALHO DO CONVIDADO</b></span>
+      <Workspace nameWorkspace="Metrocomm" nameCard="Drag and Drop"/>
     </div>
     )
  
